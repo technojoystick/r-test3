5ed63902dd3994878de5d29b4f6386bcfac2a1d5
technojoystick <technojoystick@gmail.com>
2019-07-31 21:27:12 +0300

    flow type helper

--- src/helpers/getOneRandomNumber.js
+++ src/helpers/getOneRandomNumber.js
@@ -1,4 +1,5 @@
-const getOneRandomNumber = ({ min = 0, max } = {}) => {
+// @flow
+const getOneRandomNumber = <T: {min?: number, max: number}>({ min = 0, max }:T = {}): number => {
     return (Math.floor(min + Math.random() * (max + 1 - min)));
 };
 
